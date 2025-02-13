329ea7c59ca020b7e13a9d791d4ad56caa0f02c1
soyfidelfm <fidel_filio@hotmail.com>
2025-02-12 22:12:36 -0800

    Using Microsoft Copilot to Generate Blazor Code

---
+++ Models/EventModel.cs
@@ -0,0 +1,6 @@
+public class EventModel
+{
+    public string Name { get; set; } = string.Empty;
+    public DateTime Date { get; set; } = DateTime.Now;
+    public string Location { get; set; } = string.Empty;
+}
